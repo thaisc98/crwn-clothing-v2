1fdc5ebb38c7e23cb9a142598a26326ff3d95181
thaisc98 <thaisc98@hotmail.es>
2024-10-26 00:20:47 -0400

    [ADD] redux-thunk

---
+++ src/components/spiner/spinner.component.jsx
@@ -0,0 +1,8 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.style";
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+);
+export default Spinner;
